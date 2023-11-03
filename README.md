85403db4ea8978841db578a428503549a9fc50e9
afnleaf <afnleafow@gmail.com>
2023-11-02 22:44:06 -0400

    project stucture init

---
+++ README.md
@@ -0,0 +1,33 @@
+# pokemon auto calculator
+automatically calculate every matchup between two pokemon pastes
+print out results as text
+click button for paste to clipboard
+upload to hastebin/pastebin
+currently gen 9 only
+
+## stack
+bun?
+smogon/damage-calc
+js
+css
+html
+
+## ideas
+team length limits for calculation speed requirements
+generation picker or just 9?
+allow more than 4 moves
+singles vs doubles
+default vs standard meta
+pikalytics api?
+
+## frontend ui design
+idkman
+chill with normalize for now?
+center submit button
+put paste boxes side by side
+flexbox stack on eachother
+both pastes should fit side by side
+drag?
+scroll
+alt-scroll?
+
---
+++ scrip.js
@@ -0,0 +1,26 @@
+function calculate() {
+    // check if teams are valid
+    // return notice to user that teams are not valid
+    readTextBox1();
+    readTextBox2();
+}
+
+function readTextBox1() {
+    // Get the textarea element by its id
+    var textBox1 = document.getElementById("textBoxLeft");
+    // Get the value of the textarea
+    var paste = textBox1.value;
+    console.log(paste)
+
+    // Display the value in a paragraph or perform any further processing
+    //document.getElementById("output").textContent = "Textarea content: " + textareaValue;
+}
+
+function readTextBox2() {
+    // Get the textarea element by its id
+    var textBox2 = document.getElementById("textBoxRight");
+    // Get the value of the textarea
+    var paste = textBox2.value;
+    console.log(paste)
+}
+
