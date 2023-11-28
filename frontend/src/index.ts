fbaef7bd47d3ac9b079e1771f82bfd056ac72d2a
afnleaf <afnleafow@gmail.com>
2023-11-28 03:16:38 -0500

    using serve-static-bun for frontend

---
+++ frontend/src/index.ts
@@ -0,0 +1,14 @@
+
+/*
+import { Elysia } from "elysia";
+
+const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
+*/
+
+import serveStatic from "serve-static-bun";
+
+Bun.serve({ fetch: serveStatic("public") });
